7d84bbd745ccc4048a76e9bb3b70606fb9ea5dd6
Ayodele <ayodelea@proxynetgroup.com>
2023-11-30 11:05:47 +0100

    merged with Samuel

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ import cors from'cors'
 import user from './routes/User.js'
 import cookieParser from'cookie-parser'
 import upload from './routes/Upload.js'
+import competition from './routes/Competition.js'
 import bodyParser from 'body-parser';
+import session from 'express-session';
+//import logger from './logger/logger.js';
+
 
 dotenv.config()
 
@@ -15,12 +19,23 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
+//app.use(session({secret: 'ispace101',saveUninitialized: true,resave: true}));
 
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(session({
+    secret: 'ispace101',
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: true//false 
+}));
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.use('/api/user', user)
-app.use("/api/product", upload)
+app.use('/user', user)
+app.use("/competition", competition)
 
 
 
@@ -46,3 +61,83 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>{
     console.log('server is running on port', PORT)
 })
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.get('/index', (req, res) => {
+  res.render('index')
+});
+
+/*
+app.get('/success', (req, res) => {
+  //logger.info('home route');
+  res.render('success')
+});
+*/
+
+app.get('/logout',(req,res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+app.get('/competitions-details', (req, res) => {
+  res.render('competitions-details')
+});
+
+app.get('/coding-details', (req, res) => {
+  res.render('coding-details')
+});
+
+app.get('/ccoding-details', (req, res) => {
+  res.render('coding-details')
+});
+
+app.get('/design-details', (req, res) => {
+  res.render('design-details')
+});
+
+app.get('/app-details', (req, res) => {
+  res.render('app-details')
+});
+
+app.get('/virtual', (req, res) => {
+  res.render('virtual')
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+});
+
+app.get('/intra-school', (req, res) => {
+  res.render('intra-school')
+});
+
+app.get('/inter-school', (req, res) => {
+  res.render('inter-school')
+});
+
+app.get('/virtual', (req, res) => {
+  res.render('virtual')
+});
+
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+
+
+
+app.get('/aboutproject', (req, res) => {
+  //logger.info('virtual route');
+  res.render('aboutproject')
+});
+
+
+//app.use('/api', require('./routes/routes'));
+
+// request to handle undefined or all other routes
+app.get('*', (req, res) => {
+  res.render('home');
+});
